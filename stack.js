b1a6726031d3943cc055c3f6e62e4cb60c1d756c
WeeTeck <lee.weetecx@gmail.com>
2023-05-12 18:34:24 +0800

    revert: :rewind: Revert Changes & Deployment

--- stack.js
+++ stack.js
@@ -1,5 +1,85 @@
+// export default class Stack {
+//   constructor() {
+//     this.items = [];
+//   }
+
+//   /**
+//    * - Add element to top of stack
+//    */
+//   push(element) {
+//     this.items.push(element);
+//   }
+
+//   /**
+//    * - Return and remove top element in stack
+//    * - Return "Underflow" if stack is empty
+//    */
+//   pop() {
+//     if (this.items.length == 0) {
+//       return "Underflow";
+//     }
+//     return this.items.pop();
+//   }
+
+//   /**
+//    * - Check top element in stack w/o removing it
+//    */
+//   peek() {
+//     return this.items[this.items.length - 1];
+//   }
+
+//   // helper methods
+//   isEmpty() {
+//     return this.items.length == 0;
+//   }
+
+//   /**
+//    * - Prints out items in stack
+//    */
+//   print() {
+//     var str = "";
+//     for (var i = 0; i < this.items.length; i++) {
+//       str += this.items[i] + " ";
+//     }
+//     return str;
+//   }
+
+//   /**
+//    * - Returns length of stack
+//    * - Returns 0 if stack is empty
+//    */
+//   length() {
+//     return this.items.length;
+//   }
+// }
+
+// export default class Stack {
+//   constructor() {
+//     this.items = [];
+//     this.length = 0;
+//   }
+
+//   push(item) {
+//     this.items.push(item);
+//     this.length++;
+//   }
+
+//   pop() {
+//     this.length--;
+//     return this.items.pop();
+//   }
+
+//   peek() {
+//     return this.items[this.items.length - 1];
+//   }
+
+//   isEmpty() {
+//     return this.items.length === 0;
+//   }
+// }
+
 // Creates a stack
-export class Stack {
+export default class Stack {
   constructor() {
     this.items = [];
   }
@@ -37,3 +117,25 @@ export class Stack {
     return this.items.length;
   }
 }
+
+
+// // creating object for stack class
+// var stack = new Stack();
+
+// // Adding element to the stack
+// stack.push(10);
+// stack.push(20);
+// stack.push(30);
+
+// // Printing the stack element
+// // prints [10, 20, 30]
+// console.log(stack.print());
+
+// // returns 30
+// console.log(stack.peek());
+
+// // returns 30 and remove it from stack
+// console.log(stack.pop());
+
+// // returns [10, 20]
+// console.log(stack.print());
